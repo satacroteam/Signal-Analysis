cd13776fb83b1b54bd0e6566632d1340e632f6e1
HOLA <hola@macbook-pro-de-hola.home>
2018-11-25 16:07:38 +0100

    Spectogram and chromagram

--- sandbox.py
+++ sandbox.py
@@ -1,18 +1,31 @@
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+#
+# from pyAudioAnalysis import audioBasicIO, audioFeatureExtraction
+#
+# # Import data
+# [Fs, x] = audioBasicIO.readAudioFile("data/AS003-a-tenu-9010H42.WAV")
+# F = audioFeatureExtraction.stFeatureExtraction(x, Fs, 0.050*Fs, 0.025*Fs)
+# plt.subplot(2, 1, 1)
+# plt.plot(F[0][0, :])
+# plt.xlabel('Frame no')
+# plt.ylabel('ZCR')
+#
+# plt.subplot(2, 1, 2)
+# plt.plot(F[0][1, :])
+# plt.xlabel('Frame no')
+# plt.ylabel('Energy')
+#
+# plt.show()
+#
 
-from pyAudioAnalysis import audioBasicIO, audioFeatureExtraction
+from pyAudioAnalysis.audioAnalysis import fileSpectrogramWrapper, fileChromagramWrapper
+from pyAudioAnalysis.audioFeatureExtraction import beatExtraction
 
-# Import data
-[Fs, x] = audioBasicIO.readAudioFile("data/AS003-a-tenu-9010H42.WAV")
-F = audioFeatureExtraction.stFeatureExtraction(x, Fs, 0.050*Fs, 0.025*Fs)
-plt.subplot(2, 1, 1)
-plt.plot(F[0][0, :])
-plt.xlabel('Frame no')
-plt.ylabel('ZCR')
+# Spectrogram
+fileSpectrogramWrapper("data/AS003-a-tenu-9010H42.WAV")
 
-plt.subplot(2, 1, 2)
-plt.plot(F[0][1, :])
-plt.xlabel('Frame no')
-plt.ylabel('Energy')
+# Chromagram
+fileChromagramWrapper("data/AS003-a-tenu-9010H42.WAV")
 
-plt.show()
+
+
